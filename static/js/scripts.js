b1eedddde05b260ebe9c50c371eb78f9238981ef
BesTLP <281885807@qq.com>
2025-04-11 16:58:41 +0800

    update.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'awards', 'internship', 'project', 'interest']
+const section_names = ['home', 'internship', 'project','awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
